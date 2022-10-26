6045190b282198cd1eb140aab56689e291429fb4
Allen <avalmercafe02@gmail.com>
2022-10-25 22:59:38 -0400

    filter

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,18 @@
 import React from "react";
 
 
-function Header () {
+function Header ({handleDarkModeClick,isDarkMode}) {
     return (
-        console.log("hello")
+        <div className={"App " + (isDarkMode ? "dark" : "light")}>
+      <header>
+        <h2>Shopster</h2>
+        <button onClick={handleDarkModeClick}>
+          {isDarkMode ? "Dark" : "Light"} Mode
+        </button>
+      </header>
+      
+    </div>
+        
     )
 }
 
